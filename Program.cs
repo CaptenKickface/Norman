07ee96979631a20a27eaf9bc4234dea34950d334
Tobias Sol <insightfortheblind@gmail.com>
2021-01-22 18:45:42 -0600

    True Start

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Norman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The beginning begins.");
         }
     }
 }
