978f47348bdf0e69d41823520eeef53833b698d7
Tobias Sol <insightfortheblind@gmail.com>
2021-01-22 18:39:58 -0600

    Initializing

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Norman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
